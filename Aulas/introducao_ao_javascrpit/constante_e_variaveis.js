3db587c2d049c78922b33a77d66334e74eb67fba
adrian-colombo <adrian.colombo01@gmail.com>
2021-12-21 19:14:48 -0300

    add aula introducao ao js

---
+++ Aulas/introducao_ao_javascrpit/constante_e_variaveis.js
@@ -0,0 +1,7 @@
+// Constante geralmente são declaradas em letras maiúsculas.
+// As constantes não são manípulaveis.
+// As variáveis são manípulaveis.
+
+const PRECO = 2;
+var desconto = 0.2;
+var total = PRECO - desconto;
---
+++ Aulas/introducao_ao_javascrpit/funcao.js
@@ -0,0 +1,6 @@
+function soma(a,b){
+//   console.log(a + b); // Quando só quero ver o resultado.
+    return a + b; // Quando precisa manipular depois o resultado dessa função em outra função.
+}
+
+soma(3,5);
