1bea54d3d10ab4cef3dc640171a59473683ad56b
adrian-colombo <adrian.colombo01@gmail.com>
2021-12-22 00:45:03 -0300

    .

--- Aulas/introducao_ao_javascript/js-pagweb/assets/js/script.js
+++ Aulas/introducao_ao_javascript/js-pagweb/assets/js/script.js
@@ -20,7 +20,7 @@ function decrement() {
     currentNumber = currentNumber - 1;
     currentNumberWrapper.innerHTML = currentNumber;
     }
-    
+
     if(currentNumber < 0){
         document.getElementById("currentNumber").style.color="red";
     }  else if(currentNumber >= 0){
@@ -35,5 +35,4 @@ document.getElementById("decrement").addEventListener("click",decrement);
 /*  Implementados,
     addEventListiner invés de onclick;
     limite de contar até o 10 (negativo e positivo);
-    o número fica vermelho se o valor for negativo.
-*/
+    o número fica vermelho se o valor for negativo. */
